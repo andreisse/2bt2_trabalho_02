728604924243015c9d9ee70abb45041a2eeb2ca4
andreisse <86690998+andreisse@users.noreply.github.com>
2022-06-21 19:46:15 -0300

    Update 01.java

--- 01.java
+++ 01.java
@@ -1,23 +1,23 @@
-package Trabalhos_poo.Trabalho_lista;
+package Trabalhos_poo.trabalho_02;
 
 public class App {
     public static void main(String[] args) {
-        PessoaDAO pDao = new PessoaDAO();
+        PersonDAO pDao = new PersonDAO();
         int iP1 = 1;
         String nP1 = "Lúcia";
-        Pessoa pessoa1 = new Pessoa(iP1, nP1);
-        pDao.addPessoa(pessoa1);
+        Person person1 = new Person(iP1, nP1);
+        pDao.addPerson(person1);
         int iP2 = 2;
         String nP2 = "Fabio";
-        Pessoa pessoa2 = new Pessoa(iP2, nP2);
-        pDao.addPessoa(pessoa2);
+        Person person2 = new Person(iP2, nP2);
+        pDao.addPerson(person2);
         int iP3 = 3;
         String nP3 = "Fabrício";
-        Pessoa pessoa3 = new Pessoa(iP3, nP3);
-        pDao.addPessoa(pessoa3);
-        System.out.println(pDao.getPessoas());
-        pDao.deletePessoa(pessoa2);
-        System.out.println(pDao.getPessoas());
+        Person person3 = new Person(iP3, nP3);
+        pDao.addPerson(person3);
+        System.out.println(pDao.getPerson());
+        pDao.deletePerson(person2);
+        System.out.println(pDao.getPerson());
         pDao.getById(iP1);
         pDao.getById(iP2);
         
