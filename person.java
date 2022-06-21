131936d8b2c23361b3d3c00f951253fb141da80a
andreisse <86690998+andreisse@users.noreply.github.com>
2022-06-21 19:40:09 -0300

    Create person.java

---
+++ person.java
@@ -0,0 +1,29 @@
+package Trabalhos_poo.Trabalho_lista;
+
+public class Pessoa {
+    private int id;
+    private String name;
+    
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return "Id: " + id + ", Nome = " + name;
+    }
+
+    
+}
